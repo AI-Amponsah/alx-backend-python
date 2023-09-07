1c9e0839d7e2374dd043d8d853983f3300ce1def
AI-Amponsah <Yawamp27@gmail.com>
2023-09-07 14:09:24 +0000

    wait

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,6 @@ async def wait_random(max_delay: int = 10) -> float:
     named wait_random that waits for a random delay between 0 and max_delay 
     (included and float value) seconds and eventually returns it.
     """
-    await asyncio.sleep(random.uniform(0, max_delay))
-    return random.uniform(0, max_delay)
-
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
