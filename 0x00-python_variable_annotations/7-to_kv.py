b202211e045431398fc8dcc34218bf2e100e8c52
AI-Amponsah <Yawamp27@gmail.com>
2023-09-07 08:01:03 +0000

    modified 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" """
+""" Annotated function """
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union(int, float))->Tuple[str, float]:
-    """ """
-    return (k, v**2)
+def to_kv(k: str, v: Union(int, float)) -> Tuple[str, float]:
+    """ takes str and int or float, returns str, square of v as float  """
+    return k, v**2
