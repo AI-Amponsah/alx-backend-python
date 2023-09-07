56ad4405b1c14137b50d4b00c2e76b2a9d5583ca
AI-Amponsah <Yawamp27@gmail.com>
2023-09-07 08:16:24 +0000

    worked

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,6 +3,6 @@
 import math
 
 
-def floor(n: float)->int:
+def floor(n: float) -> int:
     """"""
     return math.floor(n)
--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" """
+"""Anotating variables"""
 
 a:int = 1
 pi: float = 3.14
